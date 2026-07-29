cebb7b9f16f1f7681366fb84a77408ccc52c02cd
Vinicius Araujo <vinidev.morais@gmail.com>
2026-07-29 12:41:17 -0300

    feat: create unit test for policy

---
+++ internal/policy/policy_test.go
@@ -0,0 +1,38 @@
+package policy
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBlockLifeCycle(t *testing.T) {
+	now := time.Now()
+
+	activeBlock := Block{
+		Domain:    "youtube.com",
+		StartedAt: now,
+		ExpiresAt: now.Add(1 * time.Hour),
+	}
+
+	if !activeBlock.IsActive() {
+		t.Errorf("block should be active")
+	}
+
+	if activeBlock.CanUnblock() {
+		t.Errorf("block should not be unblocked")
+	}
+
+	expiredBlock := Block{
+		Domain:    "youtube.com",
+		StartedAt: now.Add(-2 * time.Hour),
+		ExpiresAt: now.Add(-1 * time.Hour),
+	}
+
+	if expiredBlock.IsActive() {
+		t.Errorf("block should not be active")
+	}
+
+	if expiredBlock.CanUnblock() {
+		t.Errorf("block should not be unblocked")
+	}
+}
